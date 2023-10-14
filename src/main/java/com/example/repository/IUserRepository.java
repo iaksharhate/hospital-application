c1f74d5ffd283cb621d9c7ccb8cd5c9aea27bcc0
iaksharhate <iaksharhate@gmail.com>
2023-10-14 12:45:08 +0530

    added : get user by id api and custom exceptions

---
+++ src/main/java/com/example/repository/IUserRepository.java
@@ -0,0 +1,18 @@
+package com.example.repository;
+
+import com.example.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Integer> {
+
+    @Query(value = "select * from user where email = :email and password = :password", nativeQuery = true)
+    Optional<User> userLogin(String email, String password);
+
+    @Query(value = "select * from user where id = :id", nativeQuery = true)
+    User findUserById(int id);
+}
